4046ec9fcf7a0c09dd78f04b48e726fdb700d68f
Ibrahim Aljuhani <105425272+IbrahimAljuhani@users.noreply.github.com>
2026-08-05 00:26:49 +0300

    Add files via upload

--- services/services.sh
+++ services/services.sh
@@ -45,6 +45,7 @@ declare -A SERVICE_FILES=(
     [vikunja]="docker-compose.yml"
     [linkstack]="docker-compose.yml"
     [jellyfin]="docker-compose.yml"
+    [home-assistant]="docker-compose.yml"
     [nextcloud]="docker-compose.yml"
     [n8n]="docker-compose.yml init-data.sh"
     [taiga]="docker-compose.yml docker-compose-inits.yml taiga-gateway/taiga.conf i18n-overrides/django-ar.po i18n-overrides/locale-ar.json i18n-overrides/apply-front-locale.sh"
